679eca3b4bc85af90e2cb012d9855cff5efb40d1
huuhghhgyg <huuhghhgyg@outlook.com>
2024-09-07 12:21:37 +0800

    再次修改网页图标显示

--- js/script.js
+++ js/script.js
@@ -45,7 +45,8 @@ function SayHi() {
 }
 
 function setFavicon(emoji){
-    let svgUrl = `data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:80;%22>${emoji}</text></svg>`
+    // let svgUrl = `data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:80;%22>${emoji}</text></svg>`
+    let svgUrl = `data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 110"><text x="50%" y="55%" style="dominant-baseline:middle;text-anchor:middle;font-size:90px;">${emoji}</text></svg>`
 
     // 设置 favicon
     let link = document.getElementById("favicon");
